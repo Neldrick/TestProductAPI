cc85dfe1da38759c4a970642f48ee20263ff04ff
Neldrick <neldrick01@gmail.com>
2025-06-09 19:57:02 +0100

    initial commit

---
+++ OnyxProductAPI/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers(); // Add controllers
+builder.Services.AddEndpointsApiExplorer(); // Enable API explorer for Swagger
+builder.Services.AddSwaggerGen(); // Add Swagger generation
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Enable Swagger middleware
+    app.UseSwaggerUI(); // Enable Swagger UI
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization(); // Add authorization middleware
+
+app.MapControllers(); // Map controllers
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
